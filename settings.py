d02e9946c407ba3f16e4565023a09be07f5b066a
Khoa Nguyen <khoatn527@gmail.com>
2026-04-22 20:48:05 -0500

    Settings file

---
+++ settings.py
@@ -0,0 +1,14 @@
+class Settings:
+    """A simple settings class for game screen configuration."""
+
+    def __init__(self, width=1600, height=900, bg_color=(230, 230, 230)):
+        """Initialize screen settings.
+
+        Args:
+            width (int): Screen width in pixels.
+            height (int): Screen height in pixels.
+            bg_color (tuple): RGB background color.
+        """
+        self.screen_width = width
+        self.screen_height = height
+        self.bg_color = bg_color
