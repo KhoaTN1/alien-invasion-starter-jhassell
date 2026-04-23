b1fd8f128519eb4c4773e26f530daa15569ada05
Khoa Nguyen <khoatn527@gmail.com>
2026-04-22 21:00:44 -0500

    adjusting speed again

--- ship.py
+++ ship.py
@@ -26,7 +26,7 @@ class Ship:
         self.rect.y -= 10
 
         # Movement attributes
-        self.speed = 7.0
+        self.speed = 5.85
         self.moving_right = False
         self.moving_left = False
 
